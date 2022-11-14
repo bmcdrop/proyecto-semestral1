d507e6268ea8c43d9db3e07ed1ff7463d02af45a
bmcdrop <br.jimenez@duocuc.cl>
2022-11-14 18:58:59 -0300

    preload y delay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CustomPreloadStrategy } from './custom-preload';
 
 const routes: Routes = [
   {
@@ -32,7 +33,10 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: CustomPreloadStrategy })
+  ],
+  providers:[
+    CustomPreloadStrategy
   ],
   exports: [RouterModule]
 })
