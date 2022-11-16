66a11150d6a6a02d3507ec6e95c5073e5774ce88
bmcdrop <br.jimenez@duocuc.cl>
2022-11-16 17:51:55 -0300

    coneccion firebase y form

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController, LoadingController,IonButton } from '@ionic/angular';
+import { AuthService} from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,14 +10,40 @@ import { LoadingController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private loadingCtrl: LoadingController) { }
+  credentials!:FormGroup;
+
+  constructor(private loadingCtrl: LoadingController,
+    private formBuilder:FormBuilder,
+    private alertCtrl: AlertController,
+    private authService: AuthService,
+    private router: Router,) { }
 
   
   ngOnInit() {
 
     this.showLoading();
+    this.createForm();
 
   }
+
+  get email(){
+    return this.credentials?.get('email');
+  }
+
+  get password(){
+    return this.credentials?.get('password')
+  }
+
+  createForm(){
+    this.credentials = this.formBuilder.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required,Validators.minLength(6)]],
+
+    })
+  }
+
+  login(){}
+
 
   async showLoading() {
     const loading = await this.loadingCtrl.create({
@@ -26,5 +54,28 @@ export class LoginPage implements OnInit {
     loading.present();
   }
 
+  async register(){
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+    const user = await this.authService.register(this.credentials.value.email,this.credentials.value.password)
+    await loading.dismiss();
+
+  }
+
+  if(user){
+    this.router.navigateByUrl('/home');
+  }
+  else(){
+    this.alertPresent('Registro fallido','revise bien los datos ingresados e intentelo nuevamente');
+  }
+
+async alertPresent(header:string,message:string){
+  const alert = await this.alertCtrl.create({
+    header:header,
+    message:message,
+    buttons:['OK']
+  });
+  alert.present();
+}
 
 }
