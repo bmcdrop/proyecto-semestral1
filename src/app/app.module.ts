bf607072669a480cba4411f61eb830108e217512
bmcdrop <88555534+bmcdrop@users.noreply.github.com>
2022-11-16 21:07:32 -0300

    base de datos lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,37 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { initializeApp } from '@angular/fire/app';
+import { environment } from 'src/environments/environment';
+import { Capacitor } from '@capacitor/core';
+import { getAuth, indexedDBLocalPersistence, initializeAuth } from '@firebase/auth';
+import { provideAuth} from '@angular/fire/auth';
+import { provideFirebaseApp} from '@angular/fire/app';
+import { provideFirestore, getFirestore} from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { getApp } from 'firebase/app';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,
+    provideFirebaseApp(()=> initializeApp(environment.firebase)),
+    provideAuth(()=>{
+      if(Capacitor.isNativePlatform()){
+        return initializeAuth(getApp(),{
+          persistence:indexedDBLocalPersistence,
+        });
+      }
+      else{
+        return getAuth();
+      }
+    }),
+    provideFirestore(()=> getFirestore()),
+    provideStorage(()=>getStorage()),
+
+  ],
   providers: [
               { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
               
