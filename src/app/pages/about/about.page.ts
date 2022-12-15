924ab061f5ec4d86e3bb9c129d43d6398c3bd0b1
bmcdrop <88555534+bmcdrop@users.noreply.github.com>
2022-12-15 04:17:16 -0300

    ultimo retoque

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -14,23 +14,23 @@ export class AboutPage implements OnInit {
     {
       id:1,
       name: 'Bruno Jimenez',
-      homeworld:'Jefe de proyectos',
-      foto:'https://pbs.twimg.com/media/FeVm4HjXEAY_qEz?format=png&name=small',
+      homeworld:'Conductor',
+      foto:'https://www.elindependiente.com/wp-content/uploads/2021/11/Hector-Tejero-scaled.jpg',
       descripcion:'Desarrollador full Stack, especialista en desarrollo de aplicaciones moviles, gestión de proyectos y base de datos.',
       instagram:'https://www.instagram.com/bmc_drop/?hl=es-la',
     },
     {
       id:2,
       name:'Matias Saldivia',
-      homeworld:'Jefe de proyectos',
-      foto:'https://pbs.twimg.com/media/FeVm4HjXgAA3MMb?format=jpg&name=large',
+      homeworld:'Conductor',
+      foto:'https://xsgames.co/randomusers/assets/avatars/male/74.jpg',
       descripcion:'Desarrollador full Stack, especialista en desarrollo de paginas web, gestión agil de proyectos.',
       instagram:'https://www.instagram.com/mob._ss/?hl=es-la',
     },
     {
       id:3,
       name:'Tony Stark',
-      homeworld:'Diseñador grafico',
+      homeworld:'Conductor',
       foto:'https://assets.puzzlefactory.pl/puzzle/387/881/original.webp',
       descripcion:'Desarrollador full Stack, especialista en desarrollo de paginas web, gestión agil de proyectos.',
       instagram:'https://www.instagram.com/mob._ss/?hl=es-la',
@@ -38,9 +38,9 @@ export class AboutPage implements OnInit {
     {
       id:4,
       name:'Natasha Romanoff',
-      homeworld:'Programadora',
+      homeworld:'Conductor',
       foto:'https://upload.wikimedia.org/wikipedia/en/thumb/f/f6/Scarlett_Johansson_as_Black_Widow.jpg/220px-Scarlett_Johansson_as_Black_Widow.jpg',
-      descripcion:'Desarrollador full Stack, especialista en desarrollo de paginas web, gestión agil de proyectos y prostituto.',
+      descripcion:'Conductor desde los 5 años sigue viivendo con su madre y le gusta el anime',
       instagram:'https://www.instagram.com/mob._ss/?hl=es-la',
     }
     
